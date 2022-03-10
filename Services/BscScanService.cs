071932ea8b8825ce132b1d22409084093158f3cb
Vincent <vincent@creakif.com>
2022-03-10 01:47:56 +0100

    Holders stat disabled for now

--- Services/BscScanService.cs
+++ Services/BscScanService.cs
@@ -18,7 +18,6 @@ public class BscScanService
 
     public BscScanService(TimeZoneService timeZoneService)
     {
-        _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1");
         _timeZoneService = timeZoneService;
     }       
     

7e3b5ca169bee002ba1935b6064c3e0450d65e6b
vcheminot <vincent.cheminot@rolling-funds.com>
2022-03-16 18:49:44 +0100

    Added custom script to handle blazor reconnection

---
+++ wwwroot/public/js/blazor.js
@@ -0,0 +1,43 @@
+﻿async function connectionDown(options) {
+    console.log("Connection DOWN!");
+    for (let i = 0; i < options.maxRetries; i++) {
+        await this.delay(options.retryIntervalMilliseconds);
+
+        if (this.isDisposed) {
+            break;
+        }
+
+        try {
+            const result = await window.Blazor.reconnect();
+            if (result) {
+                // Reconnected!
+                return;
+            } else {
+                console.error("Server Rejected");
+            }
+        }
+        catch {
+        }
+    }
+
+    location.reload();
+}
+
+function delay(durationInMs) {
+    return new Promise(resolve => setTimeout(resolve, durationInMs));
+}
+
+function connectionUp(e) {
+    console.log("Connection UP!");
+}
+
+window.Blazor.start({
+    reconnectionOptions: {
+        maxRetries: 30,
+        retryIntervalMilliseconds: 500,
+    },
+    reconnectionHandler: {
+        onConnectionDown: e => connectionDown(e),
+        onConnectionUp: e => connectionUp(e)
+    }
+});

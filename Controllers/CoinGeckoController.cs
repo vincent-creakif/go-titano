f8f52119902c1f311b982c2e1cd9ad58ecd874b1
vcheminot <vincent.cheminot@rolling-funds.com>
2022-03-24 17:29:01 +0100

    Updated API to make sure we get all the history

--- Controllers/CoinGeckoController.cs
+++ Controllers/CoinGeckoController.cs
@@ -21,11 +21,15 @@ public class CoinGeckoController : ControllerBase
     public async Task<ActionResult> GetMonthlyMarketChartAsync(CancellationToken ct)
     {
         var now = DateTime.UtcNow;
+        // For the first day of the month, we make sure to get all the history of the previous month
+        if (now.Day == 1)
+        {
+            now = now.AddMonths(-1);
+        }
         foreach (var currency in CurrenciesGroups.CurrenciesAvailable)
         {
-            var month = new DateTimeOffset(new DateTime(2021, 11, 1));
-
-            var totalMonths = (now.Year * 12 + now.Month) - (month.Year * 12 + month.Month);
+            var month = new DateTimeOffset(new DateTime(now.Year, now.Month, 1));
+            var totalMonths = ((now.Year * 12 + now.Month) - (month.Year * 12 + month.Month)) + 1;
             for (var i = 0; i < totalMonths; i++)
             {
                 await _coinGeckoService.GetMonthlyMarketChartAsync(Coins.Titano, currency, month, ct);

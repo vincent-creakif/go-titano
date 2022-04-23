1df0c4bfbdeef3d1ea795a3bf85b8aaffb84106f
Vincent <vincent@creakif.com>
2022-04-23 23:05:16 +0200

    Fixed new contract address

--- Constants/Coins.cs
+++ Constants/Coins.cs
@@ -6,7 +6,7 @@ public static class Coins
 
     public static readonly Dictionary<string, CoinMetadata> Metadata = new()
     {
-        { Titano, new("0X4E3CABD3AD77420FF9031D19899594041C420AEE", "binance-smart-chain") }
+        { Titano, new("0x4E3CABD3AD77420FF9031D19899594041C420AEE", "binance-smart-chain") }
     };
 
     public record CoinMetadata(
